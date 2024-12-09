3d63a0a41b811292aea27861447f5494d905b440
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-12-09 07:59:01 +0100

    update utils/redis: corrected formatting using eslint

--- utils/redis.js
+++ utils/redis.js
@@ -2,34 +2,31 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (err) => console.log(err));  
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(err));
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
+  async get(key) {
+    const val = await this.getAsync(key);
+    return val;
+  }
 
-    async get(key) {
-        return await this.getAsync(key);
-    }
+  async set(key, val, duration) {
+    await this.setAsync(key, val, 'EX', duration);
+  }
 
-
-    async set(key, val, duration) {
-        await this.setAsync(key, val, 'EX', duration);
-    }
-
-
-    async del(key) {
-        await this.delAsync(key)
-    }
+  async del(key) {
+    await this.delAsync(key);
+  }
 }
 
-const redisClient = new RedisClient()
+const redisClient = new RedisClient();
 module.exports = redisClient;
