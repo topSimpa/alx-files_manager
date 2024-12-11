2157e90d6ecafc7579aeafed1b2cfa7468dd5cb5
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-12-11 11:45:07 +0100

    let see

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,36 +5,20 @@ class UsersController {
   static postNew(req, res) {
     const { email, password } = req.body;
 
-<<<<<<< HEAD
-// <<<<<<< HEAD
-    // return dbClient.addUsers(email, password)
-    //   .then((id) => res.status(201).json({ id, email }))
-    //   .catch((error) => res.status(400).json({ error: error.message }));
-//   }
-// 
-//   static getMe(req, res) {
-    // const token = req.get('X-Token');
-    // const id = redisClient.get(`auth_${token}`);
-    // if (!id) {
-    //   return res.status(401).json({ error: 'Unauthorized' });
-    // }
-    // return dbClient.findUserById(id).then((result) => {
-    //   const { _id, email } = result;
-    //   res.json({ id: _id, email });
-    // }).catch((error) => res.status(401).json({ error: error.message }));
-=======
     return dbClient.addUsers(email, password).then((id) => res.status(201).json({ id, email }))
       .catch((error) => res.status(400).json({ error: error.message }));
   }
->>>>>>> parent of fc0d2a1 (formated UsersController.js)
 
-    dbClient.addUsers(email, password).then((id) => {
-      res.status(201);
-      res.json({ email, id });
-    }).catch((error) => {
-      res.status(400);
-      res.json({ error: error.message });
-    });
+  static getMe(req, res) {
+    const token = req.get('X-Token');
+    const id = redisClient.get(`auth_${token}`);
+    if (!id) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    return dbClient.findUserById(id).then((result) => {
+      const { _id, email } = result;
+      res.json({ id: _id, email });
+    }).catch((error) => res.status(401).json({ error: error.message }));
   }
 }
 
